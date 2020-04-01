9991f13b23c72475593a44ede8b9e22b40c2e79c
zhang <872055049@qq.com>
2020-04-01 11:06:53 +0800

    第一次提交

--- src/main/java/com/esure/springbootdemo/SpringBootDemoApplication.java
+++ src/main/java/com/esure/springbootdemo/SpringBootDemoApplication.java
@@ -2,10 +2,17 @@ package com.esure.springbootdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringBootDemoApplication {
 
+	@RequestMapping("jenkins")
+	public String hello(){
+		return "张朋---测试自动部署";
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootDemoApplication.class, args);
 	}
