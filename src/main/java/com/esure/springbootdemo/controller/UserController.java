14834d119675beaa1a16825509e3cd421821dfa4
zhang <872055049@qq.com>
2020-04-01 16:19:13 +0800

    修改2

--- src/main/java/com/esure/springbootdemo/controller/UserController.java
+++ src/main/java/com/esure/springbootdemo/controller/UserController.java
@@ -19,6 +19,8 @@ public class UserController {
         Map<String,Object> map = new HashMap<>();
         map.put("name","张朋");
         map.put("age",38);
+        map.put("address","贵州省贵阳市白云区");
         return map;
     }
+
 }
