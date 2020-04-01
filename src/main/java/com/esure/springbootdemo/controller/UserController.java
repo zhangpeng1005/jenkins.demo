f63e3e09e984a67113df5f50c666b08caa4f152d
zhang <872055049@qq.com>
2020-04-01 15:32:09 +0800

    修改

---
+++ src/main/java/com/esure/springbootdemo/controller/UserController.java
@@ -0,0 +1,24 @@
+package com.esure.springbootdemo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @Description：
+ * @Author 张朋
+ * @Date 2020-04-01 15:29
+ */
+@RestController
+public class UserController {
+
+    @RequestMapping("/")
+    public Map index(){
+        Map<String,Object> map = new HashMap<>();
+        map.put("name","张朋");
+        map.put("age",38);
+        return map;
+    }
+}
